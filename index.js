edb746024d8d512b6992d895cbe087cb513bc5db
alexandre <agrosjean007@gmail.com>
2021-11-03 10:44:19 -0400

    completed assignment 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function saturdayFun(activity = 'roller-skate') {
     return `This Saturday, I want to ${activity}!`;
 }
+saturdayFun('bathe my dog')
 
 function mondayWork(activity = 'go to the office') {
     return `This Monday, I will ${activity}.`;
